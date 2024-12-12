61ceb680fe443d79cb087a2f1e159be5664f5213
Mitch <mitchell.cuthbertson@co-flo.co.za>
2024-12-12 09:24:48 +0200

    Day 12, part 1

--- advent of code/main.ts
+++ advent of code/main.ts
@@ -1,3 +1,5 @@
+import { memoise } from "./helpers.ts";
+
 // Define the path to the text file
 const filePath = "./input.txt";
 
@@ -5,90 +7,61 @@ let inputString = (await Deno.readTextFile(filePath)) as string;
 
 const lines = inputString.split("\r\n");
 
-type operator = "*" | "+";
+type key = `${number},${number}`;
+const map: Record<key, string> = {};
+type plot = { area: number; perimeter: number };
+const perimeters: Record<string, number> = {};
+type permiter = {
+  left: boolean;
+  right: boolean;
+  top: boolean;
+  bottom: boolean;
+};
 
-const permutator = (inputArr: operator[]) => {
-  let result: string[] = [];
+lines.forEach((line, lineIndex) => {
+  Array.from(line).forEach((char, charIndex) => {
+    map[`${charIndex},${lineIndex}`] = char;
+  });
+});
 
-  const permute = (arr: operator[], m: operator[] = []) => {
-    if (arr.length === 0) {
-      result.push(m.join());
-    } else {
-      for (let i = 0; i < arr.length; i++) {
-        let curr = arr.slice();
-        let next = curr.splice(i, 1);
-        permute(curr.slice(), m.concat(next));
-      }
+const checked: Record<key, boolean> = {};
+
+let recursivelyCheckPlot = (pos: `${number},${number}` = "0,0") => {
+  const char = map[pos];
+  if (checked[pos]) {
+    return { area: 0, perimeter: 0 };
+  }
+  checked[pos] = true;
+  const [x, y] = pos.split(",").map(Number);
+  let area = 1;
+  let perimeter = perimeters[pos] || 0;
+
+  const left = `${x - 1},${y}`;
+  const right = `${x + 1},${y}`;
+  const top = `${x},${y - 1}`;
+  const bottom = `${x},${y + 1}`;
+
+  [left, right, top, bottom].forEach((newPos) => {
+    const [i, j] = newPos.split(",").map(Number);
+    if (map[`${i},${j}`] === char) {
+      const { area: returnedArea, perimeter: returnedPerimeter } =
+        recursivelyCheckPlot(`${i},${j}`);
+      area += returnedArea;
+      perimeter += returnedPerimeter;
     }
-  };
+  });
 
-  permute(inputArr);
-
-  return Array.from(new Set(result));
+  console.log(area, perimeter);
+  return { area, perimeter };
 };
 
 let sum = 0;
-
-const nums = inputString.split(" ").map((n) => parseInt(n));
-
-let newNums = [...nums];
-
-const map: Record<number, number> = Object.values(nums).reduce(
-  (acc, curr) => ({ ...acc, [curr]: acc[curr] ? acc[curr] + 1 : 1 }),
-  {} as Record<number, number>
-);
-
-// const recursivelyCheckNums = (nums: number[], depth: number = 0) => {
-//   if (depth === 75) {
-//     return nums.length;
-//   }
-//   console.log(depth);
-//   console.log(nums);
-//   const results: number[] = nums.map((num: number) => {
-//     const numString = num.toString();
-//     if (num === 0) {
-//       return recursivelyCheckNums([1], depth + 1);
-//     } else if (numString.length % 2 === 0) {
-//       return recursivelyCheckNums(
-//         [
-//           +numString.slice(0, numString.length / 2),
-//           +numString.slice(numString.length / 2, numString.length),
-//         ],
-//         depth + 1
-//       );
-//     } else {
-//       return recursivelyCheckNums([num * 2024], depth + 1);
-//     }
-//   });
-//   return results.reduce((acc: number, cur: number) => acc + cur, 0);
-// };
-
-let newMap: Record<number, number> = { ...map };
-for (let i = 1; i <= 75; i++) {
-  const newMapStep: Record<number, number> = {};
-  Object.entries(newMap).forEach(([num, count]) => {
-    const numString = num;
-    if (+num === 0) {
-      newMapStep[1] = newMapStep[1] ? newMapStep[1] + count : count;
-    } else if (numString.length % 2 === 0) {
-      const firstHalf = +numString.slice(0, numString.length / 2);
-      const secondHalf = +numString.slice(
-        numString.length / 2,
-        numString.length
-      );
-      newMapStep[firstHalf] = newMapStep[firstHalf]
-        ? newMapStep[firstHalf] + count
-        : count;
-      newMapStep[secondHalf] = newMapStep[secondHalf]
-        ? newMapStep[secondHalf] + count
-        : count;
-    } else {
-      newMapStep[+num * 2024] = newMapStep[+num * 2024]
-        ? newMapStep[+num * 2024]! + count
-        : count;
-    }
+lines.forEach((line, lineIndex) => {
+  Array.from(line).forEach((char, charIndex) => {
+    const { area, perimeter } = recursivelyCheckPlot(
+      `${charIndex},${lineIndex}`
+    );
+    sum += area * perimeter;
   });
-  newMap = { ...newMapStep };
-}
-
-console.log(Object.values(newMap).reduce((acc, curr) => acc + curr, 0));
+});
+console.log(sum);
