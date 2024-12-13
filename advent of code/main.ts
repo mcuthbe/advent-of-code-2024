7ce5a254310444e4fa79e6ba254d0baeb736cce2
Mitch <mitchell.cuthbertson@co-flo.co.za>
2024-12-13 18:02:09 +0200

    Day 12, part 2

--- advent of code/main.ts
+++ advent of code/main.ts
@@ -5,52 +5,115 @@ const filePath = "./input.txt";
 
 let inputString = (await Deno.readTextFile(filePath)) as string;
 
-const machines = inputString.split("\n\r");
+const lines = inputString.split("\r\n");
+type locationKey = "top" | "bottom" | "left" | "right";
 
-type pos = { x: number; y: number };
+type Key = `${number},${number}`;
+const map: Record<Key, string> = {};
+type PerimeterMap = Record<`${number}a` | `${number}b`, Set<number>>;
 
-type pushedTally = { a: number; b: number };
+lines.forEach((line, lineIndex) => {
+  Array.from(line).forEach((char, charIndex) => {
+    map[`${charIndex},${lineIndex}`] = char;
+  });
+});
+
+const checked: Record<Key, boolean> = {};
+
+const recursivelyCheckPlot = (
+  pos: `${number},${number}` = "0,0",
+  horizontalPerimeterMap: PerimeterMap,
+  verticalPerimeterMap: PerimeterMap
+) => {
+  const char = map[pos];
+  if (checked[pos]) {
+    return { area: 0, perimeters: {}, sides: 0 };
+  }
+  checked[pos] = true;
+  const [x, y] = pos.split(",").map(Number);
+  let area = 1;
+
+  const locations: Record<locationKey, Key> = {} as Record<locationKey, Key>;
+
+  locations["left"] = `${x - 1},${y}`;
+  locations["right"] = `${x + 1},${y}`;
+  locations["top"] = `${x},${y - 1}`;
+  locations["bottom"] = `${x},${y + 1}`;
+
+  if (map[locations["left"]] !== char) {
+    verticalPerimeterMap[`${x}a`] ??= new Set();
+    verticalPerimeterMap[`${x}a`].add(y);
+  }
+  if (map[locations["right"]] !== char) {
+    verticalPerimeterMap[`${x}b`] ??= new Set();
+    verticalPerimeterMap[`${x}b`].add(y);
+  }
+  if (map[locations["top"]] !== char) {
+    horizontalPerimeterMap[`${y}a`] ??= new Set();
+    horizontalPerimeterMap[`${y}a`].add(x);
+  }
+  if (map[locations["bottom"]] !== char) {
+    horizontalPerimeterMap[`${y}b`] ??= new Set();
+    horizontalPerimeterMap[`${y}b`].add(x);
+  }
+
+  Object.entries(locations).forEach(([_key, newPos]) => {
+    const [i, j] = newPos.split(",").map(Number);
+    if (map[`${i},${j}`] === char) {
+      const { area: returnedArea } = recursivelyCheckPlot(
+        `${i},${j}`,
+        horizontalPerimeterMap,
+        verticalPerimeterMap
+      );
+
+      area += returnedArea;
+    }
+  });
+
+  return { area };
+};
 
 let sum = 0;
 
-machines.forEach((machine, index) => {
-  console.log(index);
-  const prizeX =
-    +(machine.match(/X=\d+/g)?.[0].match(/\d+/g)?.[0] ?? "") + 10000000000000;
-  const prizeY =
-    +(machine.match(/Y=\d+/g)?.[0].match(/\d+/g)?.[0] ?? "") + 10000000000000;
-  const buttonALine = machine.match(/Button A.*/g)?.[0] ?? "";
-  const buttonBLine = machine.match(/Button B.*/g)?.[0] ?? "";
-  const aX = +(buttonALine.match(/\d+/g)?.[0] ?? "");
-  const aY = +(buttonALine.match(/\d+/g)?.[1] ?? "");
-  const bX = +(buttonBLine.match(/\d+/g)?.[0] ?? "");
-  const bY = +(buttonBLine.match(/\d+/g)?.[1] ?? "");
-  const prizePos = { x: prizeX, y: prizeY };
-  // can be graphed y = ax + c and y = bx +d
-  // intersection x = (d - c)/(a - n)
-  const a = -aX / bX;
-  const c = prizePos.x / bX;
-  const b = -aY / bY;
-  const d = prizePos.y / bY;
-  if (a === b) {
-    // lines parallel
-    return;
-  }
-  const intersectionX = Math.round((d - c) / (a - b));
-  //x can be used to find y from either line
-  const intersectionY = Math.round(a * intersectionX + c);
+lines.forEach((line, lineIndex) => {
+  Array.from(line).forEach((char, charIndex) => {
+    let sides = 0;
+    const horizontalPerimeterMap: PerimeterMap = {};
+    const verticalPerimeterMap: PerimeterMap = {};
+    const { area } = recursivelyCheckPlot(
+      `${charIndex},${lineIndex}`,
+      horizontalPerimeterMap,
+      verticalPerimeterMap
+    );
+    console.log(char);
+    console.log(horizontalPerimeterMap, verticalPerimeterMap);
 
-  console.log(Math.round(intersectionX), Math.round(intersectionY));
+    Object.values(horizontalPerimeterMap).forEach((perimetersY) => {
+      const perimetersYArray = Array.from(perimetersY).toSorted(
+        (a, b) => a - b
+      );
+      perimetersYArray.forEach((y, index) => {
+        // if the y location of the x perimeter is not adjacent to the previous y location, it's a new side
+        if (y - 1 !== perimetersYArray[index - 1]) {
+          sides++;
+        }
+      });
+    });
 
-  if (
-    intersectionX * aX + intersectionY * bX !== prizePos.x ||
-    intersectionX * aY + intersectionY * bY !== prizePos.y
-  ) {
-    return;
-  }
-  console.log(intersectionX, intersectionY);
-  console.log(a, b, c, d);
-  sum += intersectionX * 3 + intersectionY;
+    Object.values(verticalPerimeterMap).forEach((perimetersX) => {
+      const perimetersXArray = Array.from(perimetersX).toSorted(
+        (a, b) => a - b
+      );
+      perimetersXArray.forEach((x, index) => {
+        if (x - 1 !== perimetersXArray[index - 1]) {
+          sides++;
+        }
+      });
+    });
+
+    console.log("sides " + sides);
+    console.log("area " + area);
+    sum += area * sides;
+    console.log(sum);
+  });
 });
-
-console.log(sum);
