8b035b831c15ee24290b663d7f90dd1ed42715f7
Mitch <mitchell.cuthbertson@co-flo.co.za>
2024-12-14 07:32:19 +0200

    Day 14, part 1

--- advent of code/main.ts
+++ advent of code/main.ts
@@ -6,114 +6,76 @@ const filePath = "./input.txt";
 let inputString = (await Deno.readTextFile(filePath)) as string;
 
 const lines = inputString.split("\r\n");
-type locationKey = "top" | "bottom" | "left" | "right";
 
-type Key = `${number},${number}`;
-const map: Record<Key, string> = {};
-type PerimeterMap = Record<`${number}a` | `${number}b`, Set<number>>;
+const width = 101;
+const height = 103;
+const seconds = 100;
 
-lines.forEach((line, lineIndex) => {
-  Array.from(line).forEach((char, charIndex) => {
-    map[`${charIndex},${lineIndex}`] = char;
-  });
+type Coords = { x: number; y: number };
+const positions: Record<number, Coords> = {};
+const velocities: Record<number, Coords> = {};
+
+lines.forEach((line, index) => {
+  const [left, right] = line.split(" ");
+  const [x, y] = left.split("=")[1].split(",").map(Number);
+  const [vX, vY] = right.split("=")[1].split(",").map(Number);
+  const id = index;
+  positions[id] = { x, y };
+  velocities[id] = { x: vX, y: vY };
 });
 
-const checked: Record<Key, boolean> = {};
-
-const recursivelyCheckPlot = (
-  pos: `${number},${number}` = "0,0",
-  horizontalPerimeterMap: PerimeterMap,
-  verticalPerimeterMap: PerimeterMap
-) => {
-  const char = map[pos];
-  if (checked[pos]) {
-    return { area: 0, perimeters: {}, sides: 0 };
-  }
-  checked[pos] = true;
-  const [x, y] = pos.split(",").map(Number);
-  let area = 1;
-
-  const locations: Record<locationKey, Key> = {} as Record<locationKey, Key>;
-
-  locations["left"] = `${x - 1},${y}`;
-  locations["right"] = `${x + 1},${y}`;
-  locations["top"] = `${x},${y - 1}`;
-  locations["bottom"] = `${x},${y + 1}`;
-
-  if (map[locations["left"]] !== char) {
-    verticalPerimeterMap[`${x}a`] ??= new Set();
-    verticalPerimeterMap[`${x}a`].add(y);
-  }
-  if (map[locations["right"]] !== char) {
-    verticalPerimeterMap[`${x}b`] ??= new Set();
-    verticalPerimeterMap[`${x}b`].add(y);
-  }
-  if (map[locations["top"]] !== char) {
-    horizontalPerimeterMap[`${y}a`] ??= new Set();
-    horizontalPerimeterMap[`${y}a`].add(x);
-  }
-  if (map[locations["bottom"]] !== char) {
-    horizontalPerimeterMap[`${y}b`] ??= new Set();
-    horizontalPerimeterMap[`${y}b`].add(x);
-  }
-
-  Object.entries(locations).forEach(([_key, newPos]) => {
-    const [i, j] = newPos.split(",").map(Number);
-    if (map[`${i},${j}`] === char) {
-      const { area: returnedArea } = recursivelyCheckPlot(
-        `${i},${j}`,
-        horizontalPerimeterMap,
-        verticalPerimeterMap
-      );
-
-      area += returnedArea;
+for (let i = 1; i <= seconds; i++) {
+  Object.entries(velocities).forEach(([id, velocity]) => {
+    const position = positions[+id];
+    position.x += velocity.x;
+    position.y += velocity.y;
+    if (position.x < 0) {
+      position.x = position.x + width;
+    }
+    if (position.y < 0) {
+      position.y = position.y + height;
+    }
+    if (position.x >= width) {
+      position.x = position.x - width;
+    }
+    if (position.y >= height) {
+      position.y = position.y - height;
     }
   });
+}
 
-  return { area };
-};
+console.log(positions);
 
-let sum = 0;
+const quadrants: Record<1 | 2 | 3 | 4, number> = { 1: 0, 2: 0, 3: 0, 4: 0 };
 
-lines.forEach((line, lineIndex) => {
-  Array.from(line).forEach((char, charIndex) => {
-    let sides = 0;
-    const horizontalPerimeterMap: PerimeterMap = {};
-    const verticalPerimeterMap: PerimeterMap = {};
-    const { area } = recursivelyCheckPlot(
-      `${charIndex},${lineIndex}`,
-      horizontalPerimeterMap,
-      verticalPerimeterMap
-    );
-    console.log(char);
-    console.log(horizontalPerimeterMap, verticalPerimeterMap);
-
-    Object.values(horizontalPerimeterMap).forEach((perimetersY) => {
-      const perimetersYArray = Array.from(perimetersY).toSorted(
-        (a, b) => a - b
-      );
-      perimetersYArray.forEach((y, index) => {
-        // if the y location of the x perimeter is not adjacent to the previous y location, it's a new side
-        if (y - 1 !== perimetersYArray[index - 1]) {
-          sides++;
-        }
-      });
-    });
-
-    Object.values(verticalPerimeterMap).forEach((perimetersX) => {
-      const perimetersXArray = Array.from(perimetersX).toSorted(
-        (a, b) => a - b
-      );
-      perimetersXArray.forEach((x, index) => {
-        if (x - 1 !== perimetersXArray[index - 1]) {
-          sides++;
-        }
-      });
-    });
-
-    console.log("sides " + sides);
-    console.log("area " + area);
-    sum += area * sides;
-    console.log(sum);
-  });
+Object.entries(positions).forEach(([id, position]) => {
+  console.log(position);
+  if (
+    position.x < Math.ceil(width / 2) - 1 &&
+    position.y < Math.ceil(height / 2) - 1
+  ) {
+    quadrants[1] = (quadrants[1] || 0) + 1;
+    console.log("1");
+  } else if (
+    position.x > Math.ceil(width / 2) - 1 &&
+    position.y < Math.ceil(height / 2) - 1
+  ) {
+    quadrants[2] = (quadrants[2] || 0) + 1;
+    console.log("2");
+  } else if (
+    position.x < Math.ceil(width / 2) - 1 &&
+    position.y > Math.ceil(height / 2) - 1
+  ) {
+    quadrants[3] = (quadrants[3] || 0) + 1;
+    console.log("3");
+  } else if (
+    position.x > Math.ceil(width / 2) - 1 &&
+    position.y > Math.ceil(height / 2) - 1
+  ) {
+    quadrants[4] = (quadrants[4] || 0) + 1;
+    console.log("4");
+  }
 });
+
+console.log(quadrants[1], quadrants[2], quadrants[3], quadrants[4]);
+console.log(quadrants[1] * quadrants[2] * quadrants[3] * quadrants[4]);
