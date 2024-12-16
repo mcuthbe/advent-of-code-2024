3fc8d062d666354593ac4a4a77c3c85e91a3d1f5
Mitch <mitchell.cuthbertson@co-flo.co.za>
2024-12-16 11:50:59 +0200

    Day 16, part 1

--- advent of code/main.ts
+++ advent of code/main.ts
@@ -9,10 +9,19 @@ const [initial, moves] = inputString.split("\r\n\r\n");
 
 type Coord = { x: number; y: number };
 
-type Move = "<" | ">" | "^" | "v";
+type Map = Record<number, Record<number, string>>;
+type Direction = ">" | "<" | "^" | "v";
+type Location = { position: Coord; direction: Direction };
 
-const getNewPosFromMove = (move: Move, pos: Coord): Coord => {
-  switch (move) {
+let map: Map = {};
+let startPos: Coord = { x: 0, y: 0 };
+let endPos: Coord = { x: 0, y: 0 };
+let checked: Record<`${number},${number}`, number> = {};
+let startDirection: Direction = ">";
+type Move = "Go" | "Clockwise" | "CounterClockwise";
+
+const getNewPosFromMove = (direction: Direction, pos: Coord): Coord => {
+  switch (direction) {
     case "<":
       return { x: pos.x - 1, y: pos.y };
     case ">":
@@ -24,113 +33,130 @@ const getNewPosFromMove = (move: Move, pos: Coord): Coord => {
   }
 };
 
-type Map = Record<number, Record<number, string>>;
-let map: Map = {};
-let mapClone: Map = {};
-let checked: Record<number, Record<number, boolean>> = {};
-
-let recursivelyCheckPosAndMove = (move: Move, pos: Coord): boolean => {
-  const isChecked = checked[pos.y]?.[pos.x];
-  if (isChecked) {
-    return isChecked;
+const move = (position: Coord, type: Move, currentDir: Direction): Location => {
+  switch (type) {
+    case "Go":
+      return {
+        position: getNewPosFromMove(currentDir, position),
+        direction: currentDir,
+      };
+    case "Clockwise":
+      switch (currentDir) {
+        case "^":
+          return { position, direction: ">" };
+        case ">":
+          return { position, direction: "v" };
+        case "v":
+          return { position, direction: "<" };
+        case "<":
+          return { position, direction: "^" };
+      }
+      break;
+    case "CounterClockwise":
+      switch (currentDir) {
+        case "^":
+          return { position, direction: "<" };
+        case ">":
+          return { position, direction: "^" };
+        case "v":
+          return { position, direction: ">" };
+        case "<":
+          return { position, direction: "v" };
+      }
+      break;
   }
-  checked[pos.y] ??= {};
-  checked[pos.y][pos.x] = true;
-  const charToMove = mapClone[pos.y][pos.x];
-  const newPos = getNewPosFromMove(move, pos);
-  const charAtNewPos = mapClone[newPos.y]?.[newPos.x];
-  if (!charAtNewPos) {
-    return false;
-  }
-
-  if (charAtNewPos === "#") {
-    return false;
-  } else if (charAtNewPos === ".") {
-    mapClone[newPos.y][newPos.x] = charToMove;
-    mapClone[pos.y][pos.x] = ".";
-    if (charToMove === "@") {
-      botPos = newPos;
-    }
-    return true;
-  } else if (charAtNewPos === "[") {
-    const moveSucceededL = recursivelyCheckPosAndMove(move, newPos);
-    const moveSucceededR = recursivelyCheckPosAndMove(move, {
-      ...newPos,
-      x: newPos.x + 1,
-    });
-    if (!moveSucceededL || !moveSucceededR) {
-      return false;
-    }
-    mapClone[newPos.y][newPos.x] = charToMove;
-    mapClone[pos.y][pos.x] = ".";
-    if (charToMove === "@") {
-      botPos = newPos;
-    }
-    return true;
-  } else if (charAtNewPos === "]") {
-    const moveSucceededL = recursivelyCheckPosAndMove(move, {
-      ...newPos,
-      x: newPos.x - 1,
-    });
-    const moveSucceededR = recursivelyCheckPosAndMove(move, newPos);
-    if (!moveSucceededL || !moveSucceededR) {
-      return false;
-    }
-    mapClone[newPos.y][newPos.x] = charToMove;
-    mapClone[pos.y][pos.x] = ".";
-    if (charToMove === "@") {
-      botPos = newPos;
-    }
-    return true;
-  }
-  return false;
 };
 
-let botPos: Coord = { x: 0, y: 0 };
+const isVertical = (direction: Direction) => {
+  return direction === "^" || direction === "v";
+};
+
+const isHorizontal = (direction: Direction) => {
+  return direction === ">" || direction === "<";
+};
+
+const getOppositeDirection = (direction: Direction) => {
+  switch (direction) {
+    case "^":
+      return "v";
+    case "v":
+      return "^";
+    case ">":
+      return "<";
+    case "<":
+      return ">";
+  }
+};
+
+const getTurns = (currentDirection: Direction, finalDirection: Direction) => {
+  if (
+    (isHorizontal(currentDirection) && isHorizontal(finalDirection)) ||
+    (isVertical(currentDirection) && isVertical(finalDirection))
+  ) {
+    return 0;
+  } else return 1;
+};
+
+let minScore: number | undefined = undefined;
+
+let recursivelyCheckMoves = (currentLocation: Location, score = 0) => {
+  const directions: Direction[] = [">", "<", "^", "v"];
+  const { position: currentPosition } = currentLocation;
+  const { x: currentX, y: currentY } = currentPosition;
+
+  if (score >= checked[`${currentX},${currentY}`]) return;
+
+  checked[`${currentX},${currentY}`] = score;
+
+  directions.forEach((direction) => {
+    const newPos = getNewPosFromMove(direction, currentLocation.position);
+
+    const { x: newX, y: newY } = newPos;
+
+    const char = map[newY]?.[newX];
+
+    const turns = getTurns(currentLocation.direction, direction);
+
+    const newScore =
+      score +
+      Math.abs(newY - currentY) +
+      Math.abs(newX - currentX) +
+      1000 * turns;
+
+    if (char === "E") {
+      console.log("Found the end");
+      minScore = minScore ? Math.min(minScore, newScore) : newScore;
+      console.log(newScore);
+      return newScore;
+    } else if (char === ".") {
+      return recursivelyCheckMoves(
+        {
+          position: newPos,
+          direction: direction,
+        },
+        newScore
+      );
+    }
+  });
+  return 0;
+};
 
 initial.split("\r\n").forEach((line, lineIndex) => {
   line.split("").forEach((char, charIndex) => {
-    const wideX = 2 * charIndex;
     map[lineIndex] ??= {};
-    if (char === "@") {
-      botPos = { x: wideX, y: lineIndex };
-      map[lineIndex][wideX] = "@";
-      map[lineIndex][wideX + 1] = ".";
-    } else if (char === "O") {
-      map[lineIndex][wideX] = "[";
-      map[lineIndex][wideX + 1] = "]";
-    } else {
-      map[lineIndex][wideX] = char;
-      map[lineIndex][wideX + 1] = char;
+    if (char === "S") {
+      startPos = { x: charIndex, y: lineIndex };
+    } else if (char === "E") {
+      endPos = { x: charIndex, y: lineIndex };
     }
+    map[lineIndex][charIndex] = char;
   });
 });
 
-moves.split("").forEach((move) => {
-  if (![">", "<", "^", "v"].includes(move)) return;
+const score = recursivelyCheckMoves({ position: startPos, direction: ">" });
 
-  console.log("Moving " + move);
-
-  mapClone = JSON.parse(JSON.stringify(map));
-  checked = {};
-
-  const succeeded = recursivelyCheckPosAndMove(move as Move, botPos);
-  if (succeeded) {
-    map = mapClone;
-  }
-
-  // Object.values(map).forEach((row) => {
-  //   console.log(Object.values(row).join(""));
-  // });
+Object.values(map).forEach((row) => {
+  console.log(Object.values(row).join(""));
 });
 
-let sum = 0;
-Object.entries(map).forEach(([y, row]) => {
-  Object.entries(row).forEach(([x, char]) => {
-    if (char === "[") {
-      sum += +y * 100 + +x;
-    }
-  });
-});
-
-console.log(sum);
+console.log(minScore);
