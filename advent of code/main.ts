4c95e9d13f06b849a1da369214aef69c0d5dc30f
Mitch <mitchell.cuthbertson@co-flo.co.za>
2024-12-18 21:13:03 +0200

    Day 16, part 2 (finally)

--- advent of code/main.ts
+++ advent of code/main.ts
@@ -5,8 +5,23 @@ const filePath = "./input.txt";
 
 let inputString = (await Deno.readTextFile(filePath)) as string;
 
-const getNewPosFromMove = (move: Move, pos: Coord): Coord => {
-  switch (move) {
+const [initial, moves] = inputString.split("\r\n\r\n");
+
+type Coord = { x: number; y: number };
+
+type Map = Record<number, Record<number, string>>;
+type Direction = ">" | "<" | "^" | "v";
+type Location = { position: Coord; direction: Direction };
+
+let map: Map = {};
+let startPos: Coord = { x: 0, y: 0 };
+let endPos: Coord = { x: 0, y: 0 };
+let checked: Record<`${number},${number},${Direction}`, number> = {};
+let startDirection: Direction = ">";
+type Move = "Go" | "Clockwise" | "CounterClockwise";
+
+const getNewPosFromMove = (direction: Direction, pos: Coord): Coord => {
+  switch (direction) {
     case "<":
       return { x: pos.x - 1, y: pos.y };
     case ">":
@@ -18,83 +33,126 @@ const getNewPosFromMove = (move: Move, pos: Coord): Coord => {
   }
 };
 
-const directions = ["<", ">", "^", "v"] as Move[];
-
-const lines = inputString.split("\r\n");
-const width = 70;
-const moves = 1024;
-
-type Coord = { x: number; y: number };
-
-type Move = "<" | ">" | "^" | "v";
-
-const map: Record<number, Record<number, string>> = {};
-
-for (let i = 0; i <= width; i++) {
-  for (let j = 0; j <= width; j++) {
-    map[i] = map[i] || {};
-    map[i][j] = ".";
-  }
-}
-
-for (let i = 0; i < moves; i++) {
-  const [x, y] = lines[i].split(",").map(Number);
-  map[y][x] = "#";
-}
+const isVertical = (direction: Direction) => {
+  return direction === "^" || direction === "v";
+};
 
 const pos: Coord = { x: 0, y: 0 };
 
-Object.keys(map).forEach((y) => {
-  console.log(Object.values(map[+y]).join(""));
-});
-
-const CheckPath = (): boolean => {
-  const checked: Record<number, Record<number, boolean>> = {};
-
-  let found = false;
-  const queue: Step[] = [{ pos, steps: [] }];
-  type Step = { pos: Coord; steps: Coord[] };
-  while (!found && queue.length > 0) {
-    const firstStep = queue[0];
-    const { pos, steps } = firstStep;
-    const char = map[pos.y][pos.x];
-    if (pos.x === width && pos.y === width) {
-      found = true;
-      break;
-    }
-    if (char === ".") {
-      const validPosition = directions
-        .map((direction) => {
-          const newPos = getNewPosFromMove(direction, pos);
-          const newChar = map[newPos.y]?.[newPos.x];
-          if (newChar && newChar !== "#" && !checked[newPos.y]?.[newPos.x]) {
-            checked[newPos.y] = checked[newPos.y] || {};
-            checked[newPos.y][newPos.x] = true;
-            return newPos;
-          }
-          return undefined;
-        })
-        .filter((position) => !!position);
-      validPosition.forEach((position) => {
-        queue.push({ pos: position, steps: [...steps, position] });
-      });
-    }
-    queue.shift();
-  }
-  return found;
+const getTurns = (currentDirection: Direction, finalDirection: Direction) => {
+  if (
+    (isHorizontal(currentDirection) && isHorizontal(finalDirection)) ||
+    (isVertical(currentDirection) && isVertical(finalDirection))
+  ) {
+    return currentDirection === finalDirection ? 0 : 2;
+  } else return 1;
 };
 
-let blocked = false;
-let nextMoveIndex = moves;
-while (!blocked) {
-  const nextMove = lines[nextMoveIndex];
-  console.log(nextMove);
-  const [x, y] = nextMove.split(",").map(Number);
-  map[y][x] = "#";
-  blocked = !CheckPath();
-  // Object.keys(map).forEach((y) => {
-  //   console.log(Object.values(map[+y]).join(""));
-  // });
-  nextMoveIndex++;
+type Queue = Set<{ location: Location; steps: string; score: number }>;
+
+const scoreSeats: Record<number, string> = {};
+
+initial.split("\r\n").forEach((line, lineIndex) => {
+  line.split("").forEach((char, charIndex) => {
+    map[lineIndex] ??= {};
+    if (char === "S") {
+      startPos = { x: charIndex, y: lineIndex };
+    } else if (char === "E") {
+      endPos = { x: charIndex, y: lineIndex };
+    }
+    map[lineIndex][charIndex] = char;
+  });
+});
+
+let minScore = Infinity;
+
+const queue: Queue = new Set([
+  { location: { position: startPos, direction: ">" }, steps: ``, score: 0 },
+]);
+
+while (queue.size) {
+  if (queue.size % 10000 === 0) {
+    console.log(queue.size);
+  }
+  const current = queue.values().next().value;
+  queue.delete(current!);
+
+  if (!current) continue;
+
+  const { location, steps, score } = current;
+  const { position, direction: currentDirection } = location;
+  if (score > checked[`${position.y},${position.x},${currentDirection}`]) {
+    continue;
+  }
+  checked[`${position.y},${position.x},${currentDirection}`] = score;
+  // console.log(position);
+  const { x, y } = position;
+
+  // console.log(steps);
+  const char = map[y][x];
+
+  // console.log({ x, y, currentDirection, char, score, steps });
+  if (score > minScore) {
+    continue;
+  }
+
+  if (char === "E") {
+    console.log("found");
+    if (score < minScore) {
+      minScore = score;
+    }
+    scoreSeats[score] ??= "";
+    scoreSeats[score] = scoreSeats[score] + steps;
+  } else if (!char || char === "#") {
+    continue;
+  } else {
+    const validDirections = ([">", "<", "^", "v"] as Direction[]).filter(
+      (direction) => {
+        const newPos = getNewPosFromMove(direction, location.position);
+        // if (steps.split("|").includes(`${newPos.y},${newPos.x}`)) {
+        //   return false;
+        // }
+        const { x: newX, y: newY } = newPos;
+        const char = map[newY]?.[newX];
+        return char !== "#" && char !== undefined;
+      }
+    );
+
+    validDirections.forEach((direction) => {
+      if (!direction) return;
+      const newPos = getNewPosFromMove(direction, location.position);
+
+      const { x: newX, y: newY } = newPos;
+      const turns = getTurns(currentDirection, direction);
+
+      const newScore =
+        score + Math.abs(newY - y) + Math.abs(newX - x) + 1000 * turns;
+
+      queue.add({
+        location: { position: newPos, direction },
+        steps: steps + `|${newY},${newX}`,
+        score: newScore,
+      });
+    });
+  }
 }
-console.log(lines[nextMoveIndex - 1]);
+
+const minScoreSeatsScore = Math.min(...Object.keys(scoreSeats).map(Number));
+const seats = new Set(scoreSeats[minScoreSeatsScore].split("|"));
+
+console.log(scoreSeats);
+
+Object.values(map).forEach((row, rowIndex) => {
+  console.log(
+    Object.values(row)
+      .map((char, charIndex) => {
+        if (Array.from(seats).includes(`${rowIndex},${charIndex}`)) {
+          return "O";
+        }
+        return char;
+      })
+      .join("")
+  );
+});
+
+console.log(seats.size);
